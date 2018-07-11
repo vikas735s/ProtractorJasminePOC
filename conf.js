68c4084d786cecb21fbb065dd412033337ae3534
Vikas Kumar <ZMIVUKK@ericsson.com>
2018-07-11 15:32:20 +0530

    Committer: Vikas Kumar <ZMIVUKK@ericsson.com>

--- conf.js
+++ conf.js
@@ -1,12 +1,29 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./testCases/*.js'],
-    framework: 'jasmine',
-    jasmineNodeOpts: {
-      defaultTimeoutInterval: 30000
-    },
-    capabilities: {
-     'browserName': 'chrome',
-        
-      }
-  };
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['./testCases/*.js'],
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+  capabilities: {
+    'browserName': 'chrome',
+  },
+
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter(
+        {
+          //baseDirectory: 'C:/Temp',
+          //takeScreenShotsForSkippedSpecs: true,
+          savePath: "./report",
+          //cleanDestination:true,
+          fileName:"Report",
+          fileNameDateSuffix: true
+          //takeScreenShotsOnlyForFailedSpecs: true
+        }
+      )
+    );
+  }
+};
--- testCases/firstTestCaseForDemo.js
+++ testCases/firstTestCaseForDemo.js
@@ -1,8 +1,12 @@
-   describe('Proc Demo', function() {
-    it('title verifications', function() {
-    browser.get('https://www.udemy.com/'); 
-    expect(browser.getTitle()).toEqual('Online Courses - Learn Anything, On Your Schedule | Udemy')
-         });
-   });
+describe('Proc Demo', function () {
+      it('Udmy title verifications', function () {
+            browser.get('https://www.udemy.com/');
+            expect(browser.getTitle()).toEqual('Online Courses - Learn Anything, On Your Schedule | Udemy')
+      });
 
-   
+      it('MMT title verifications', function () {
+            browser.get('https://www.makemytrip.com/');
+            expect(browser.getTitle()).toEqual('Flight Booking, Flight Tickets Booking at Lowest Airfare | MakeMyTrip')
+      });
+});
+
