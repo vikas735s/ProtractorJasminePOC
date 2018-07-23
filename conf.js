224c49d359a94c5657578cd36a511e8f9b735f43
Vikas Kumar <ZMIVUKK@ericsson.com>
2018-07-23 12:08:26 +0530

    anges. Lines starting 	modified:   conf.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    testCases/firstTestCaseForDemo.js

--- conf.js
+++ conf.js
@@ -1,29 +1,34 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+var HtmlReporter = require('protractor-beautiful-reporter');
 
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./testCases/*.js'],
+  specs: ['testCases/*js'],
   framework: 'jasmine',
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   },
-  capabilities: {
-    browserName: 'chrome',
+
+  multiCapabilities: [
+    {
+      browserName: 'firefox',
     },
+    {
+      browserName: 'chrome',
+    }
+  ],
 
   onPrepare: function () {
-    jasmine.getEnv().addReporter(
-      new Jasmine2HtmlReporter(
-        {
-          //baseDirectory: 'C:/Temp',
-          //takeScreenShotsForSkippedSpecs: true,
-          savePath: "./report",
-          //cleanDestination:true,
-          fileName:"Report",
-          fileNameDateSuffix: true
-          //takeScreenShotsOnlyForFailedSpecs: true
-        }
-      )
-    );
-  }
+    // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: 'report',
+      docTitle: 'Protractor Web Testing Report',
+      preserveDirectory: false,
+      screenshotsSubfolder: 'images',
+      jsonsSubfolder: 'jsons',
+    }).getJasmine2Reporter());
+
+  },
+
+
 };
