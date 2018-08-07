4149bec32d49766fdb8f48b0dac332f0fde4621b
vikas <vikas735s@gmail.com>
2018-08-07 15:28:16 +0530

    POM baseline added

--- conf.js
+++ conf.js
@@ -1,35 +1,30 @@
-var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 var HtmlReporter = require('protractor-beautiful-reporter');
 
 exports.config = {
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  directConnect:true,
-  specs: ['testCases/*js'],
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  },
+	//seleniumAddress: 'http://localhost:4444/wd/hub',
+	directConnect : true,
+	specs : [ 'testCases/airtelLoginTest.js' ],
+	framework : 'jasmine',
+	jasmineNodeOpts : {
+		defaultTimeoutInterval : 2500000
+	},
 
-  multiCapabilities: [
-    {
-      browserName: 'firefox',
-    },
-    {
-      browserName: 'chrome',
-    }
-  ],
+	multiCapabilities : [ {
+	//  browserName: 'firefox',
+	}, {
+		browserName : 'chrome',
+	} ],
 
-  onPrepare: function () {
-    // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
-    jasmine.getEnv().addReporter(new HtmlReporter({
-      baseDirectory: 'report',
-      docTitle: 'Protractor Web Testing Report',
-      preserveDirectory: false,
-      screenshotsSubfolder: 'images',
-      jsonsSubfolder: 'jsons',
-    }).getJasmine2Reporter());
+	onPrepare : function() {
+		// Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+		jasmine.getEnv().addReporter(new HtmlReporter({
+			baseDirectory : 'report',
+			docTitle : 'Protractor Web Testing Report',
+			preserveDirectory : false,
+			screenshotsSubfolder : 'images',
+			jsonsSubfolder : 'jsons',
+		}).getJasmine2Reporter());
 
-  },
-
+	},
 
 };
